a088b9a53928f9bc03e4211724783625b5d3a138
projetoscintia <cpinho@tecnologia-unica.com.br>
2024-06-11 08:18:13 -0300

    segunda alteração

--- aula_api/app/status/index.php
+++ aula_api/app/status/index.php
@@ -3,16 +3,9 @@
     require_once __DIR__ . '/../../api/config.php';
     require_once __DIR__ . '/../../api/response.php';
 
-        if(API_IS_ACTIVE){
-            echo Response::resposta(200,'sucess',[
-                'version' => API_VERSION,
-                'status' => 'ATIVA'
-            ]); 
-        }else{
-            echo Response::resposta(200,'sucess',[
-                'version' => API_VERSION,
-                'status' => 'manutenção'
-            ]); 
-        }
+    echo Response::resposta(200,'sucess',[
+        'version' => API_VERSION,
+        'status' => 'manutenção'
+    ]); 
 
 ?>
